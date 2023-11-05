7e20117372f5e38f2544607c4332d034337b65a9
CruelKorleone <franrod1082@gmail.com>
2023-11-05 18:50:57 -0300

    Se toma la data del formulario

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -10,7 +10,7 @@ export const showModal = () => {
 
 export const hideModal = () => {
     modal?.classList.add('hide-modal');
-    // TODO: Form reset
+    form?.reset();
 }
 
 
@@ -35,7 +35,28 @@ export const renderModal = ( element ) => {
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        console.log('Data sent to server');
+        
+        const formData = new FormData( form );
+        const userLike = {};
+
+        for (const [key, value] of formData) {
+            if( key === 'balance' ) {
+                userLike[key] = +value;
+                continue;
+            }
+
+            if( key === 'isActive' ) {
+                userLike[key] = (value === 'on' ) ? true : false;
+                continue;
+            }
+
+            userLike[key] = value;
+        }
+
+        // console.log( userLike );
+        // TODO: Guardar usuario
+        hideModal();
+
     });
 
     element.append( modal );
